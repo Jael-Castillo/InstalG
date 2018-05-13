aaa8670e303f16f454f7f3d2c10e91a4b162a57e
Jael-Castillo <jaelcchz@outlook.com>
2018-05-13 16:40:05 -0500

    Creación de Modelo puestos y clase Puesto. Se importo en el modelo empleados la clase Puesto. Se agrego la llave foranea en el modelo empleados id_pk_puesto y en el modelo puestos se agrego la llave fonranea id_pk_empleado.

--- apps/empleados/models.py
+++ apps/empleados/models.py
@@ -1,9 +1,11 @@
+from apps.puestos.models import Puesto
 from django.db import models
 
 # Create your models here.
 class Empleado(models.Model):
 	"""docstring for Empleado"""
 	id_empleado = models.CharField(max_length=50, primary_key=True)
+	id_puesto =models.CharField(Puesto, null=False, blank=False, on_delete=models.CASCADE)
 	nombre_usuario = models.CharField(max_length=50)
 	apellido_pat = models.CharField(max_length=50)
 	apellido_mat = models.CharField(max_length=50)
@@ -12,5 +14,4 @@ class Empleado(models.Model):
 	delegacion = models.CharField(max_length=50)
 	municipio = models.CharField(max_length=50)
 	codpost = models.CharField(max_length=50)
-	nombre_puesto = models.CharField(max_length=20)
 
---
+++ apps/puestos/models.py
@@ -0,0 +1,10 @@
+from apps.empleados.models import Empleado
+from django.db import models
+
+# Create your models here.
+class Puesto(models.Model):
+	"""docstring for Puesto"""
+	id_puesto =models.CharField(max_length=50, primary_key=True)
+	id_pk_empleado: models.ForeignKey(Empleados, null=False, blank=False, on_delete=models.CASCADE)
+	nombre_puesto = models.CharField(max_length=20)
+	estado = models.BooleanField(null=True)
